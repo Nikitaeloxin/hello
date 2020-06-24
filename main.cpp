2427a5c7f6655315148872ade0749a7b0cc63ee2
Nikitaeloxin <39460384+Nikitaeloxin@users.noreply.github.com>
2020-06-24 23:28:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include "test.h"
 
-int main(){
-	std::cout << "Hello world" << std::endl;
-	return 0;
+
+int main(int argc, char *argv[])
+{
+    string str1,str2,str3;
+    int a;
+    vector<Person> P;
+
+    cout << " add - add Person to Vec end; " <<endl
+         << "del - delete element from end;" << endl
+         << "list - output all list;" << endl
+         << "end - close" << endl;
+
+    while(str1 != "end"){
+        cin >> str1;
+        if(str1 == "add"){
+            cin >> str2 >> str3 >> a;
+            P.push_back(Person(str2,str3,a));
+        }
+        if(str1 == "list"){
+            for (int i = 0; i < P.size();i++)
+                cout << P[i];
+        }
+    }
+    return 0;
 }
