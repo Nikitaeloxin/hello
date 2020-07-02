5b5b51e222b706e0761cb7a3538d3e3b9a788f91
Nikitaeloxin <39460384+Nikitaeloxin@users.noreply.github.com>
2020-07-02 21:49:58 +0300

    Update test.h

--- test.h
+++ test.h
@@ -6,36 +6,71 @@
 
 using namespace std;
 
+//----------------------------------------------------CLASS Person------------------------------------------------------------------
+
 class Person{
 public:
-	Person(string ff, string ss, int aa)
-	:first_name(ff), second_name(ss), age(aa){}
-	Person(){}
-	void set_age(int a){ age = a; }
-	int get_age(){ return age; }
-
-	void set_firt_name(string f){ first_name = f; }
-	string get_first_name(){ return first_name; }
-
-	void set_second_name(string s){ second_name = s; }
-	string get_second_name(){ return second_name; }
+	Person(string ff, string ss="unknown", int aa=0)
+		:first_name(ff), second_name(ss), age(aa){}
+	Person()
+		:first_name("unknown"), second_name("unknown"), age(0){}
+	
+	friend ostream& operator<<(ostream& os, Person& p);		//OUTPUT PERSON
+	friend istream& operator>>(istream& is, Person& p);		//INPUT PERSON
+	friend bool operator<=(Person& p1, Person& p2);		//COMPARE PERSON IN SEARCH
 private:
 	string first_name;
 	string second_name;
 	int age;
 
 };
+//----------------------------------------------------END CLASS Person------------------------------------------------------------------
 
-ostream& operator<<(ostream& os, Person& p);			//OUTPUT PERSON
-istream& operator>>(istream& is, Person& p);			//INPUT PERSON
 
-void read_from_storage(string s, vector<Person>& p);	//READING INFORMATION FROM STORAGE
-void write_to_storage(string s, vector<Person>& p);		//WRITING INFORMATION TO STORAGE
+//-----------------------------------------------TEMPLATE CLASS PositionList------------------------------------------------------------
+template <class T> class PositionList{
+public:
+	void push_back( T t);		//ADD ELEMENT TO END OF LIST		
+	void del_element(int i);	//DELETE SELECTED ELEMENT
+	void list();				//OUTPUT LIST OF ELEMENTS
+	void search(T s);			//SEARCH AMONG ELEMENTS OF LIST
+private:
+	vector<T> V;
+};
+//----------------------------------------------------END TEMPLATE CLASS PositionList------------------------------- -------------------------
 
-void info();											//INFORMATION BLOCK
-void add_element(vector<Person>& P);					//INSTRUCRION ADD 
-void del_element(vector<Person>& P, int a);				//INSTRUCRION DEL
-void list_of_elements(vector<Person>& P);				//LIST OF ELEMENTS
-void search_element(vector<Person>& P, string s);		//SEARCH POSITION
-void exec_prog(vector<Person>& P);						//EXECUTE PROGRAM
 
+//--------------------------------------TEMPLATE CLASS PositionList METHODS IMPLEMETATIONS----------------------------------------------------
+
+template <class T> void PositionList<T>::push_back(T t){
+	V.push_back(t);
+}
+
+template <class T> void PositionList<T>::del_element(int i){
+	V.erase(V.begin() + i - 1);
+}
+
+template <class T> void PositionList<T>::list() {
+	for (int i = 0; i < V.size(); i++)
+		cout << V[i]<<endl;
+}
+
+template <class T> void PositionList<T>::search(T s) {
+	bool indicate = false;					//FOR INDICATE NOT FOUND ANY ELEMENTS
+	for (int i = 0; i < V.size(); i++){
+		if (s <= V[i]){
+			cout << V[i] << endl;
+			indicate = true;
+		}
+	}
+	if (!indicate)
+		cout << "!--no elements found--!\n" << endl;
+}
+
+//----------------------------------------------------------END METHODS IMPLEMETATIONS----------------------------------------------------------
+
+
+//----------------------------------------------------------OTHER FUNCTIONS---------------------------------------------------------
+void info();
+
+//----------------------------------------------------------END OTHER FUNCTIONS-----------------------------------------------------
